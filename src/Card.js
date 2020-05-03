46ff1651dc86693f3ba20c0833ff9cd43bbc5bea
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-05-03 22:17:06 +0530

    Added CSS

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "tachyons"
+
+const Card = (props) => {
+    return (
+        <div className="tc ">
+            <img src="https://robohash.org/test2" alt="Robot Photos" />
+            <div>
+                <h2>Name</h2>
+                <p>Email</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
