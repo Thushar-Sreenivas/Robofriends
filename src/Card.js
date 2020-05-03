86f221c759695b3afb4303d40d6d2bb4812b26c6
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-05-04 00:31:18 +0530

    Added Name

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "tachyons"
-
+import *  from './robotsDetails'
 const Card = (props) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
