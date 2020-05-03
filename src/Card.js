29c7717b4d01254354ae2481e36ad67201e548a7
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-05-03 22:17:26 +0530

    Added CSS

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import "tachyons"
 
 const Card = (props) => {
     return (
-        <div className="tc ">
+        <div className="tc bg-light-green">
             <img src="https://robohash.org/test2" alt="Robot Photos" />
             <div>
                 <h2>Name</h2>
