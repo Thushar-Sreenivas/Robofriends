9d3f8c832897bc0fb4889279413250898475045e
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-05-05 01:49:39 +0530

    Added two way Binding

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import {robots} from './Robot/RobotsDetails'
 
 class App extends Component {
   
+  state = {
+    searchValue: ''
+  }
+
+  searchResultHandler = (event) => {
+      this.setState({searchValue: event.target.value})
+  }
+
   render() {
     return (
       <div>
         <h1 className="lh-title tc">RoboFriends</h1>
-        <form class="pa4 black-80">
-          <input id="name" class="center input-reset ba b--black-20 pa2 mb2 db w-70 " type="text" aria-describedby="name-desc"></input>
+        <form class="pa4 black-80" onSubmit={this.searchResultHandler}>
+          <input id="name" class="center input-reset ba b--black-20 pa2 mb2 db w-70 " type="text" aria-describedby="name-desc" value={this.state.searchValue}></input>
           <searchBar id={1} robots={robots}/>
         </form>
-        {/* <button className="flex items-center center f1 lh-title tc bg-light-green dib br4 pa3 ma3 grow bw3 shadow-5 fw6 link dim " 
-            onClick={this.robotToggleHandler}>Click me to see Awesome Robots</button> */}
+        
         <CardList robots={robots}/>
       </div>
     
@@ -21,3 +28,9 @@ class App extends Component {
     }
   }
 export default App;
+
+
+
+
+{/* <button className="flex items-center center f1 lh-title tc bg-light-green dib br4 pa3 ma3 grow bw3 shadow-5 fw6 link dim " 
+            onClick={this.robotToggleHandler}>Click me to see Awesome Robots</button> */}
