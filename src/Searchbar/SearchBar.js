1326f857678ce5831817a043fc558bc27dc63871
Thushar-Sreenivas <37865381+Thushar-Sreenivas@users.noreply.github.com>
2020-05-05 01:54:16 +0530

    2 way binding

--- src/Searchbar/SearchBar.js
+++ src/Searchbar/SearchBar.js
@@ -2,7 +2,17 @@ import React from 'react'
 import cardList from '../Cards/CardList'
 
 const searchBar = ({id, robots}) => {
-    
+    const cardArray = robots.map((user, index) => {
+        return (
+            <Card 
+                key={robots[index].id} 
+                id={robots[index].id} 
+                name={robots[index].name} 
+                email={robots[index].email}
+            />
+        )
+    })
+    return cardArray
 }
 
 export default searchBar
